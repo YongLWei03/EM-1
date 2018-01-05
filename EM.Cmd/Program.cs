022bae0b591460c5918146de044fe362db550d49
name <email@email.com>
2018-01-05 08:21:14 +0100

    init

---
+++ EM.Cmd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EM.Cmd
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Common.Logging.ILog logger = Common.Logging.LogManager.GetLogger("asd");
+
+      logger.Info("hello world");
+    }
+  }
+}
