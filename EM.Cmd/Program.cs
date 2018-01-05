668ddd52c119ec68a40272743630f0e808c71b8a
name <email@email.com>
2018-01-05 08:37:53 +0100

    task

--- EM.Cmd/Program.cs
+++ EM.Cmd/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EM.Cmd
@@ -10,9 +11,45 @@ namespace EM.Cmd
   {
     static void Main(string[] args)
     {
-      Common.Logging.ILog logger = Common.Logging.LogManager.GetLogger("asd");
+      Common.Logging.ILog logger = Common.Logging.LogManager.GetLogger<Program>();
 
-      logger.Info("hello world");
+      logger.Info("Starting EM");
+
+      var tokenSource = new CancellationTokenSource();
+      CancellationToken ct = tokenSource.Token;
+
+      Task t = Task.Factory.StartNew(() =>
+      {
+        ct.ThrowIfCancellationRequested();
+
+        while (true)
+        {
+          if (ct.IsCancellationRequested)
+          {
+            ct.ThrowIfCancellationRequested();
+          }
+          Thread.Sleep(5000);
+        }
+      }, ct);
+
+      try
+      {
+        tokenSource.Cancel();
+        logger.Info("Waiting for task to finish.");
+        t.Wait();
+      }
+      catch (AggregateException e)
+      {
+        logger.Error(e.Message, e);
+        foreach (var v in e.InnerExceptions)
+        {
+          logger.Error(v.Message, v);
+        }
+      }
+      finally
+      {
+        tokenSource.Dispose();
+      }
     }
   }
 }
