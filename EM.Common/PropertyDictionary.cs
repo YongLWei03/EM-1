c4816cf3537c856ab8651c42d0310ae71f8cafa0
name <email@email.com>
2018-01-10 06:53:35 +0100

    refac

--- EM.Common/PropertyDictionary.cs
+++ EM.Common/PropertyDictionary.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EM.Common
 {
+  [Serializable]
   public class PropertyDictionary : Dictionary<string, string>
   {
+    public PropertyDictionary() : base() { }
+    protected PropertyDictionary(SerializationInfo info, StreamingContext context) : base(info, context) { }
   }
 }
