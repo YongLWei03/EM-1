c420ad381a48bd83bf363ab0f09061b853464bfd
name <email@email.com>
2018-01-12 13:59:09 +0100

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,24 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		exec: {
 			db_build: {
-				cmd: '"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe" .\\EM.DB\\EM.DB.sqlproj /t:Rebuild'
-			},
+				cmd: '"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe" .\\EM.DB\\EM.DB.sqlproj /t:Rebuild'			},
 			db_publish: {
 				cmd: '"C:\\Program Files (x86)\\Microsoft SQL Server\\140\\DAC\\bin\\SqlPackage.exe" /Action:Publish /Profile:.\\EM.DB\\EM.DB.publish.xml /SourceFile:.\\EM.DB\\bin\\Debug\\EM.DB.dacpac'
 			},
+			
 			edmx_generate: {
 				cmd:'"C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\EdmGen.exe" /mode:fullgeneration /c:"metadata=res://*/EntityDataModel.csdl|res://*/EntityDataModel.ssdl|res://*/EntityDataModel.msl;provider=System.Data.SqlClient;provider connection string="data source=hydral\\sqlexpress01;initial catalog=EM.DB;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework"" /project:EM.DBModel /entitycontainer:Entities /namespace:EM.DBModel /language:CSharp'
 			},
+			
+			copy_plugins_debug: {
+				cmd:'copy .\\EM.Plugin.Sample\\bin\\Debug\\EM.Plugin.Sample.* .\\EM.Cmd\\bin\\Debug\\',
+				exitCodes: [0,1]
+			},
+			copy_plugins_release: {
+				cmd:'copy .\\EM.Plugin.Sample\\bin\\Release\\EM.Plugin.Sample.* .\\EM.Cmd\\bin\\Release\\',
+				exitCodes: [0,1]				
+			},
+			
 			echo_stuff: {
 				cmd: function() {return 'echo hello world';}
 			}
@@ -29,6 +39,13 @@ module.exports = function(grunt) {
 				options: {
 					interrupt: true,
 					debounceDelay: 10000,
+				},
+			},
+			plugin_files: {
+				files: ['EM.Plugin.Sample/bin/Debug/*','EM.Plugin.Sample/bin/Release/*'],
+				tasks: ['exec:copy_plugins_debug','exec:copy_plugins_release'],
+				options: {
+					event: ['added'],
 				},
 			}
 		}
